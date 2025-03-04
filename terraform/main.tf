0ffa733fb10f49b1528b43b7c3412af11ccfaeae
tdthurston <tdavidthurston@gmail.com>
2025-03-03 20:33:40 -0500

    Add workflow and other optimizations

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.87.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
 module "ilios_vpc" {
 
   source             = "git::https://github.com/tdthurston/ilios-base-tf-infra.git//modules//vpc"
