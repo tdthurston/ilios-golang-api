26dfaf07b4746949e24580dab36f843365cc7e24
tdthurston <tdavidthurston@gmail.com>
2025-03-03 21:10:28 -0500

    Add workflow and other optimizations

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,6 +61,7 @@ module "ilios_oidc" {
   source      = "git::https://github.com/tdthurston/ilios-base-tf-infra.git//modules/oidc"
   github_org  = var.github_org
   github_repo = var.github_repo
+  aws_region  = var.aws_region
 
 }
 
