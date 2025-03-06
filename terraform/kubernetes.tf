fc808a2f35788536294ec74475b8a8a9056f13cf
tdhurston <tdavidthurston@gmail.com>
2025-03-06 10:10:39 -0500

    Update workflow & modify module name

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -29,9 +29,15 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.existing.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.existing.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.existing.token
+
+    exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
 }
 
-# Deploy only the k8s application
+
 module "ilios_k8s" {
   source = "./modules/k8s"
 
